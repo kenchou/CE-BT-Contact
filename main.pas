49d49658165a917fee456991da270e0f72fcf348
Ken Chou <zhang-j@klab.com>
2016-02-19 18:48:15 +0800

    read device list from BlueTooth.ini

--- main.pas
+++ main.pas
@@ -7,7 +7,7 @@ interface
 uses
   Classes, SysUtils, sqlite3conn, sqldb, db, FileUtil, Forms, Controls,
   Graphics, Dialogs, DBGrids, StdCtrls, Grids, DbCtrls, CsvDocument,
-  stringgridutil, dbgridutil;
+  stringgridutil, dbgridutil, lconvencoding, IniFiles, LazUTF8;
 
 type
 
@@ -22,27 +22,31 @@ type
     DataSource2: TDataSource;
     DBGrid1: TDBGrid;
     Label1: TLabel;
+    lstLog: TListBox;
     SQLite3Connection1: TSQLite3Connection;
     SQLQuery1: TSQLQuery;
     SQLQuery2: TSQLQuery;
     SQLTransaction1: TSQLTransaction;
-    stxtDbFile: TStaticText;
-    stxtCsvFile: TStaticText;
     StringGrid1: TStringGrid;
     procedure btnExitClick(Sender: TObject);
     procedure BtnImportClick(Sender: TObject);
+    procedure cboxDeviceChange(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
   private
     { private declarations }
     CsvDocumentContact: TCSVDocument;
+    DeviceList: TStringList;
     procedure CheckRequired(expr: Boolean; message: string);
     procedure RefreshContact(aDeviceName: string='');
     procedure CleanContact(aDeviceName: string='');
     procedure PopulateDevice;
+    procedure PopulateDeviceFromConfig;
     function GetTableMaxId(aTableName: string): Integer;
+    function GetSelectedDevice(): string;
   public
     { public declarations }
+    BtIniFile: string;
     BtDbFile: string;
     CsvFile: string;
   end;
@@ -64,38 +68,53 @@ procedure TFormMain.FormCreate(Sender: TObject);
 begin
   Left := 0;
   Top := 25;
+
+  DeviceList := TStringList.Create;
+
   // Detect DB & CSV path
-  {$IfDef WinCE}
   BtDbFile := ConcatPaths([BtAppPath, 'BT.db']);
-  {$Else}
-  BtDbFile := ConcatPaths([Application.Location, BtAppPath, 'BT.db']);
+  BtIniFile := ConcatPaths([BtAppPath, 'BlueTooth.ini']);
+
+  {$IfDef Win32}
+  BtDbFile := ConcatPaths([Application.Location, BtDbFile]);
+  BtIniFile := ConcatPaths([Application.Location, BtIniFile]);
   {$EndIf}
+
   CsvFile := ExpandFileNameUTF8(FileSearchUTF8('contact.csv', Application.Location + ';' + SearchPath));
 
-  stxtDbFile.Caption := BtDbFile;
-  stxtCsvFile.Caption := CsvFile;
+//  lstLog.Items.Clear;
+  lstLog.Items.Add('Temp Path: ' + GetTempDir);
+  lstLog.Items.Add('BT Ini File: ' + BtIniFile);
+  lstLog.Items.Add('BT Db File: ' + BtDbFile);
+  lstLog.Items.Add('Csv File: ' + CsvFile);
 
+  PopulateDeviceFromConfig;
+
+  // Check BlueTooth.ini
+  CheckRequired(FileExistsUTF8(BtIniFile), '没在指定位置找到蓝牙配置文件, 这车不是秦？' + BtDbFile);
   // Check BT.db
-  CheckRequired(FileExistsUTF8(BtDbFile), 'Cannot Found Database: ' + BtDbFile);
+  CheckRequired(FileExistsUTF8(BtDbFile), '没找到通讯录，这车不是秦？' + BtDbFile);
   // Check contact.csv
-  CheckRequired(FileExistsUTF8(CsvFile), 'Cannot Found CSV: ' + CsvFile);
+  CheckRequired(FileExistsUTF8(CsvFile), '没找到要导入的通讯录 CSV 文件。请将 Contact.csv 文件放置在 TF 卡根目录' + CsvFile);
 
   // Connecte to SQLite
   SQLite3Connection1.DatabaseName := BtDbFile;
   SQLite3Connection1.Open;
 
-  PopulateDevice;
-  RefreshContact;
+  //PopulateDevice;
+  RefreshContact(GetSelectedDevice);
 
   // Load Contact.csv
   CsvDocumentContact := TCSVDocument.Create;
   CsvDocumentContact.LoadFromFile(CsvFile);
   LoadGridFromCSVDocument(StringGrid1, CsvDocumentContact);
+
 end;
 
 procedure TFormMain.FormDestroy(Sender: TObject);
 begin
   CsvDocumentContact.Free;
+  DeviceList.Free;
 end;
 
 procedure TFormMain.CheckRequired(expr: Boolean; message: string);
@@ -106,7 +125,7 @@ begin
     Application.ShowMainForm := False;
     Application.Terminate;
     Close;
-    halt;
+    //halt;
   end;
 end;
 
@@ -124,7 +143,8 @@ begin
     exit;
   end;
 
-  aDeviceName := cboxDevice.Items[cboxDevice.ItemIndex];
+  //aDeviceName := cboxDevice.Items[cboxDevice.ItemIndex];
+  aDeviceName := GetSelectedDevice();
 
   // Delete record if check the checkbox
   if chkClear.checked then
@@ -153,8 +173,14 @@ begin
   end;
   SQLTransaction1.Commit;
 
-  RefreshContact;
+  RefreshContact(aDeviceName);
+  lstLog.Items.Add('"' + aDeviceName + '" 导入 ' + IntToStr(count) + ' 条记录.');
   ShowMessage('完成! 共导入 ' + IntToStr(count) + ' 条记录.');
+end;
+
+procedure TFormMain.cboxDeviceChange(Sender: TObject);
+begin
+  RefreshContact(DeviceList[cboxDevice.ItemIndex]);
 end;
 
 function TFormMain.GetTableMaxId(aTableName: string): Integer;
@@ -171,6 +197,11 @@ begin
     Result := SQLQuery1.Fields[0].AsInteger;
   SQLQuery1.Close;
   SQLTransaction1.Commit;
+end;
+
+function TFormMain.GetSelectedDevice(): string;
+begin
+    Result := DeviceList[cboxDevice.ItemIndex];
 end;
 
 procedure TFormMain.PopulateDevice;
@@ -190,6 +221,7 @@ begin
         or (Field.AsString = '插装车间')
         or (Field.AsString = '我们的') then continue;
     cboxDevice.Items.Add(Field.AsString);
+    DeviceList.Add(Field.AsString);
   end;
   if cboxDevice.Items.Count >= 1 then
   begin
@@ -197,6 +229,64 @@ begin
   end;
   SQLQuery1.Close;
   SQLTransaction1.Commit;
+end;
+
+procedure TFormMain.PopulateDeviceFromConfig;
+var
+  tmpFile: string;
+  StrList: TStringList;
+  Sections: TStringList;
+  Section: string;
+  DevAddr: string;
+  DevName: string;
+  IniFile: TIniFile;
+  FromEncoding: string;
+  S: string;
+begin
+  tmpFile := GetTempFileName('', 'BlueTooth.ini');
+
+  // for Test, Read Unicode File
+  StrList := TStringList.Create;
+  StrList.TextLineBreakStyle := TTextLineBreakStyle.tlbsLF;
+  try
+    StrList.LoadFromFile(BtIniFile);
+    S := StrList.Text;
+    FromEncoding := GuessEncoding(S);
+    lstLog.Items.Add('BlueTooth.ini Encoding(Guess): ' + FromEncoding);
+    S := ConvertEncoding(S, FromEncoding, EncodingUTF8);
+    StrList.Text := Utf8BomToUtf8(S);
+    StrList.SaveToFile(tmpFile);
+  finally
+    StrList.Free;
+  end;
+
+  lstLog.Items.Add('Device List from BlueTooth.ini:');
+    // Iterate sections in ini
+  Sections := TStringList.Create;
+  IniFile := TIniFile.Create(tmpFile);
+  try
+    IniFile.ReadSections(Sections);
+    for Section in Sections do
+    begin
+      if 1 = UTF8Pos('RemoteDevice', Section) then
+      begin
+        DevName := IniFile.ReadString(Section, 'DevName', '');
+        DevAddr := IniFile.ReadString(Section, 'DevAddr', '');
+        lstLog.Items.Add(DevAddr + ' : ' + DevName);
+        cboxDevice.Items.Add(DevName + '(' + DevAddr + ')');
+        DeviceList.Add(DevName);
+      end;
+    end;
+  finally
+    IniFile.Free;
+    Sections.Free;
+    DeleteFileUtf8(tmpFile);
+  end;
+
+  if cboxDevice.Items.Count >= 1 then
+  begin
+    cboxDevice.ItemIndex := 0;
+  end;
 end;
 
 procedure TFormMain.RefreshContact(aDeviceName: string='');
